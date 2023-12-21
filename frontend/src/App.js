b8de98674a68622d36cdadaadc8b78028d16bba0
QJC <772809505@qq.com>
2023-12-21 21:05:49 +0800

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+}
+
+function NotFound(){
+  return <div>这里是卡拉云的主页</div>
+}
+
+
+function Home() {
+  return <div>hello world</div>
+}
+
+const About = () => {
+  return <div>这里是卡拉云的主页</div>
+}
+
+const Dashboard = () => {
+  return <div>今日活跃用户: 42</div>
 }
 
 export default App;
+
